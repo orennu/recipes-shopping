8bc18c96a5c802de063b5de9d44bffca15f6eb7e
Oren Nudelman <orennu@gmail.com>
2020-09-23 13:01:54 +0300

    added functionality to recipes component click on recipe item will display the item properties updated some styles to components added functionality to shopping list adding items is not possible

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,11 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @Output() menuSelected = new EventEmitter<string>();
   collapsed = true;
   
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelect(menu: string) {
+    this.menuSelected.emit(menu);
   }
 
 }
--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from 'src/app/models/recipe.model';
 
 @Component({
@@ -8,14 +8,19 @@ import { Recipe } from 'src/app/models/recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   
+  @Output() recipeSelectedFromList = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('A Test recipe', 'This is simply a test', 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/26/0/FNK_Tuscan-Chicken-Skillet_H2_s4x3.jpg.rend.hgtvcom.616.462.suffix/1537973085542.jpeg'),
-    new Recipe('A Test recipe', 'This is simply a test', 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/26/0/FNK_Tuscan-Chicken-Skillet_H2_s4x3.jpg.rend.hgtvcom.616.462.suffix/1537973085542.jpeg')
+    new Recipe('A Test recipe 1', 'This is simply a test for recipe 1', 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/26/0/FNK_Tuscan-Chicken-Skillet_H2_s4x3.jpg.rend.hgtvcom.616.462.suffix/1537973085542.jpeg'),
+    new Recipe('A Test recipe 2', 'This is simply a test for recipe 2', 'https://joyfoodsunshine.com/wp-content/uploads/2016/09/easy-pizza-casserole-recipe-4-500x500.jpg')
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeSelectedFromList.emit(recipe);
   }
 
 }

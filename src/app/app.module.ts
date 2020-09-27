440284eba3947d0a8296c77d477ca4c88a7bf1dc
Oren Nudelman <orennu@gmail.com>
2020-09-28 00:34:35 +0300

    added app routing, root components routing and fixed reloading issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -26,7 +27,8 @@ import { DropdownDirective } from './directives/dropdown.directive';
     DropdownDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,16 +7,11 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  @Output() menuSelected = new EventEmitter<string>();
   collapsed = true;
   
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-  onSelect(menu: string) {
-    this.menuSelected.emit(menu);
   }
 
 }
